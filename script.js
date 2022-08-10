62da40910abcc62fb3c91ff7325c3b732a75d5a3
Nyrell Leonor <38891511+nyrellcl@users.noreply.github.com>
2022-08-10 15:18:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+//creates a fade in animation when the content is loaded
+document.addEventListener("DOMContentLoaded", () =>{
+    window.setTimeout(function(){
+        document.body.className = ''
+    }, 500);
+});
+
+//observes when the user scrolls down the page
+//when user scrolls down and intersects with the entries, the classes are then added to the elements with an animated tag
+const observer = new IntersectionObserver(entries =>{
+    entries.forEach(entry =>{
+        if(entry.isIntersecting){
+            /**this selects all the article tags with the animated class and adds the fade classes according to 
+             * the array
+             */
+            document.querySelectorAll(".animated")[0].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[1].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[2].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[3].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[4].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[5].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[6].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[7].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[8].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[9].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[10].classList.add("fadeInLeft");
+
+        }
+    })
+})
+/**calls the observer function and starts it by selecting the parent element that has the fade-container
+ * class
+ */
+observer.observe(document.querySelector(".fade-container"));
+
