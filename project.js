f5e96d284f68420e162cbb93d94fea9e64acdfe9
Nyrell Leonor <38891511+nyrellcl@users.noreply.github.com>
2022-09-30 14:28:27 -0700

    Add files via upload

---
+++ project.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    effect: "coverflow",
+    slidesPerView: "auto",
+    centeredSlides: true,
+    coverFlowEffect:{
+        rotate: 50,
+        stretch: 0,
+        depth: 200, 
+        modifier: 1,
+        slideShadows: true
+    },
+    /*effect: "cube",
+    cubeEffect:{
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 55,
+        shadowScale: 0
+    },*/
+    direction: 'horizontal',
+    loop: true,
+    speed: 500,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+      },
+});
